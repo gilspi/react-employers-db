4a92868ec3d99ac2c85310f5bbf2e9116807410b
gilspi <gilspi@email.con>
2022-09-29 17:52:31 +0300

    Add information panel

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css';
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Company employers data</h1>
+            <h2>Count employers: </h2>
+            <h3>Money get : </h3>
+        </div>
+    );
+}
+
+export default AppInfo;
