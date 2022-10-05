68a204f061bff62ec2c3690274b56adcbd9e156f
gilspi <gilspi@email.con>
2022-10-05 13:15:58 +0300

    Make filter func to change state

--- src/components/app-filter/app-filter.js
+++ src/components/app-filter/app-filter.js
@@ -1,25 +1,42 @@
+import { Component } from 'react';
 import './app-filter.css';
 
-const AppFilter = () => {
-    return (
-        <div className="btn-group">
-            <button 
-                className="btn btn-light"
-                type="button">
-                    All employees
-            </button>
-            <button 
-                className="btn btn-outline-light"
-                type="button">
-                    Employees for promotion
-            </button>
-            <button 
-                className="btn btn-outline-light"
-                type="button">
-                    Salary over 1000$
-            </button>
-        </div>
-    );
+class AppFilter extends Component {
+    render() {
+        const buttonsData = [
+            {
+                name: 'all',
+                label: 'All employees'
+            },
+            {
+                name: 'isClicked',
+                label: 'Employees for promotion'
+            },
+            {
+                name: 'moreThen1000',
+                label: 'Salary over 1000$'
+            }
+        ];
+
+        const buttons = buttonsData.map(({name, label}) => {
+            const active = this.props.active === name;
+            return (
+                <button 
+                    type="button"
+                    className={active ? "btn btn-light" : "btn btn-outline-light"}
+                    key={name}
+                    onClick={() => this.props.onFilterSelect(name)}>
+                    {label}
+                </button>
+            );
+        })
+
+        return (
+            <div className="btn-group">
+                {buttons}
+            </div>
+        );
+    }
 }
 
 export default AppFilter;
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -15,7 +15,9 @@ class App extends Component {
                 {id: 1, name: 'John C.', salary: 800, increase: false, isClicked: false},
                 {id: 2, name: 'Alex M.', salary: 3000, increase: true, isClicked: true},
                 {id: 3, name: 'Carl W.', salary: 15000, increase: false, isClicked: false}
-            ]
+            ],
+            term: '',
+            active: 'isClicked'
         }
         this.maxId = this.state.data.length+1;
     }
@@ -55,9 +57,41 @@ class App extends Component {
         }))
     }
 
+    searchEmp = (items, term) => {
+        if (term.length === 0) {
+            return items;
+        }
+        
+        return items.filter(item => {
+            return item.name.indexOf(term) > -1
+        })
+    }
+
+    onUpdateSearch = (term) => {
+        this.setState({term: term});
+    }
+
+    filterEmp = (items, active) => {
+        switch(active) {
+            case 'isClicked':
+                return items.filter(item => item.isClicked === true);
+            case 'moreThen1000':
+                return items.filter(item => item.salary > 1000);
+            default:
+                return items;
+        }
+    }
+
+    onFilterSelect = (active) => {
+        this.setState({active: active})
+    }
+
+
     render() {
+        const {data, term, active} = this.state;
         const length = this.state.data.length;
         const increased = this.state.data.filter(item => item.increase === true).length;
+        const visibleData = this.filterEmp(this.searchEmp(data, term), active);
 
         return (
             <div className="app">
@@ -66,12 +100,15 @@ class App extends Component {
                 increased={increased}/>
     
                 <div className='search-panel'>
-                    <SearchPanel/>
-                    <AppFilter/>
+                    <SearchPanel
+                     onUpdateSearch={this.onUpdateSearch}/>
+                    <AppFilter
+                    active={active}
+                    onFilterSelect={this.onFilterSelect}/>
                 </div>
     
                 <EmployersList 
-                    data={this.state.data}
+                    data={visibleData}
                     onDelete={this.deleteItem}
                     onToggleProp ={this.onToggleProp}/>
                 <EmployersAddForm onAdd={this.addItem}/>
